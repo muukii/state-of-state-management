d07187731c22a8fa6563ef476e584f299420f922
Muukii <muukii.app@gmail.com>
2026-05-11 20:20:56 +0900

    Reframe taxonomy around store and atomic graph patterns

---
+++ src/lib/build-info.ts
@@ -0,0 +1,13 @@
+const buildDate = new Date()
+
+const buildDateLabel = new Intl.DateTimeFormat('en-US', {
+  month: 'short',
+  day: 'numeric',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: '2-digit',
+  hour12: true,
+  timeZone: 'Asia/Tokyo',
+}).format(buildDate)
+
+export const lastUpdatedLabel = `Last updated ${buildDateLabel} JST`
